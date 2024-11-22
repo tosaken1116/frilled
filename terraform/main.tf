76bf96db73beb64c008c0a8f033630bfb8d53535
tosaken1116 <nagato20021122@gmail.com>
2024-11-23 05:40:46 +0900

    feat: add cloud build service account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,29 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+resource "google_service_account" "cloudbuild_service_account" {
+  account_id   = "cloudbuild-sa"
+  display_name = "cloudbuild-sa"
+  description  = "Cloud build service account"
+}
 
-# Artifact Registry を作成
+resource "google_project_iam_member" "act_as" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+}
+
+resource "google_project_iam_member" "logs_writer" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+}
 resource "google_artifact_registry_repository" "docker_repo" {
   repository_id = "sfu-token-server"
   format        = "DOCKER"
   location      = var.region
 }
 
-# Cloud Build Trigger
 resource "google_cloudbuild_trigger" "build_trigger" {
   name = "build-sfu-token-server"
 
@@ -21,6 +35,7 @@ resource "google_cloudbuild_trigger" "build_trigger" {
       branch = "^main$"
     }
   }
+  service_account = google_service_account.cloudbuild_service_account.id
 
   build {
     step {
@@ -28,7 +43,7 @@ resource "google_cloudbuild_trigger" "build_trigger" {
       args = [
         "build", "-t",
         "${var.region}-docker.pkg.dev/${var.project_id}/sfu-token-server/sfu-token-server:latest",
-        "./server/sfu-token-server"
+        "../server/sfu-token-server"
       ]
     }
     step {
@@ -44,8 +59,6 @@ resource "google_cloudbuild_trigger" "build_trigger" {
   }
 }
 
-
-# Cloud Run サービスを作成
 resource "google_cloud_run_service" "sfu_token_server" {
   name     = "sfu-token-server"
   location = var.region
@@ -53,7 +66,7 @@ resource "google_cloud_run_service" "sfu_token_server" {
   template {
     spec {
       containers {
-        image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.docker_repo.name}/sfu-token-server:latest"
+        image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.docker_repo.repository_id}/sfu-token-server:latest"
         ports {
           container_port = 8080
         }
@@ -71,9 +84,8 @@ resource "google_cloud_run_service" "sfu_token_server" {
   }
 }
 
-# Cloud Run IAM 設定 (全ユーザーに公開)
 resource "google_cloud_run_service_iam_member" "invoker" {
-  service  = google_cloud_run_service.sfu_token_server.name
+  service  = google_cloud_run_service.sfu_token_server.id
   location = google_cloud_run_service.sfu_token_server.location
   role     = "roles/run.invoker"
   member   = "allUsers"
