b02fba687d654dcf17f2becd4b8720a9f988987a
tosaken1116 <nagato20021122@gmail.com>
2024-11-23 17:09:21 +0900

    chore: delete livekit token server code and move to sfu-token-server

--- server/sfu-token-server/main.go
+++ server/sfu-token-server/main.go
@@ -1,22 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/livekit/protocol/auth"
 )
 
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "OK")
+func getJoinToken(room, identity string) string {
+	at := auth.NewAccessToken(os.Getenv("LIVEKIT_API_KEY"), os.Getenv("LIVEKIT_API_SECRET"))
+	grant := &auth.VideoGrant{
+		RoomJoin: true,
+		Room:     room,
+	}
+	at.AddGrant(grant).
+		SetIdentity(identity).
+		SetValidFor(time.Hour)
+
+	token, _ := at.ToJWT()
+	return token
 }
 
 func main() {
 	app := echo.New()
 
-	app.GET("/health", func(c echo.Context) error {
-		return c.String(http.StatusOK, "OK")
+	// cors apply allow all
+	app.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Response().Header().Set("Access-Control-Allow-Origin", "*")
+			c.Response().Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+			c.Response().Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			return next(c)
+		}
 	})
 
-	app.Logger.Fatal(app.Start(":8080"))
+	app.GET("/getToken", func(c echo.Context) error {
+		roomId := c.QueryParam("room")
+		userName := c.QueryParam("name")
+		if roomId == "" || userName == "" {
+			return c.String(http.StatusBadRequest, "room and name are required")
+		}
+		return c.String(http.StatusOK, getJoinToken(roomId, userName))
+	})
+
+	log.Fatal(app.Start(":8080"))
+
 }
